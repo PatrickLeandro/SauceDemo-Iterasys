56cd3f6e8a61b9d33fa573eec782b1ceb5fcb25b
PatrickLeandro <patrickleandro34@gmail.com>
2023-11-04 17:21:51 -0300

    primeiras configurações

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace SauceDemo_Iterasys;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
